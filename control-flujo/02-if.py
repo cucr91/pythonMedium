65ff0ded37ba89ea57ef28a0d131d371dccba684
cucr91 <camilocalderonrojas@gmail.com>
2023-08-20 11:53:24 -0400

    Se agregan archivos control de flujo

---
+++ control-flujo/02-if.py
@@ -0,0 +1,10 @@
+edad = 17
+if edad >= 50:
+    print("Puede ver la pelicula con descuento")
+elif edad >= 18:
+    print("Eres mayor de edad, puedes ver la pelicula")
+else:
+    print("No puedes ver la pelicula")
+
+
+print("Gracias por visitarnos")
---
+++ tipos/09-conversion-tipo.py
@@ -0,0 +1,12 @@
+x = input("")
+
+# int(): transforma datos a numeros (interger)
+# str(): transforma datos a string
+# float(): transforma datos a floar
+# bool() transforma datos a boleanos; los falsos por defectos son 1- string vacios, 2- ceros, 3-none.
+
+print(bool(""))
+print(bool("0"))
+print(bool(None))
+print(bool(" "))
+print(bool(0))
