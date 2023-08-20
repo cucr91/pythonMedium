12cebbae1cf2b4d285ffe308447e4a84a785532a
cucr91 <camilocalderonrojas@gmail.com>
2023-08-20 17:56:12 -0400

    Se agregan archivos, cadena comparadores y for

---
+++ control-flujo/06-for.py
@@ -0,0 +1,12 @@
+# for numero in range(5):
+#     print(numero)
+
+
+buscar = 6
+for numero in range(5):
+    print(numero)
+    if numero == buscar:
+        print("encontrado", buscar)
+        break
+else:
+    print("Numero no encontrado :(")
