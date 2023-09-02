5854dca1eec497bc31f5b36d08c4749922d16056
cucr91 <camilocalderonrojas@gmail.com>
2023-09-01 21:09:24 -0400

    Actualizacion carpeta modulos

--- modulos/usuarios/impuestos/utilidades.py
+++ modulos/usuarios/impuestos/utilidades.py
@@ -1,8 +1,12 @@
-# from ..gestion.crud import guardar
+if __name__ != "__main__":
+    from ..gestion.crud import guardar  # import relativo
+    from usuarios.gestion.crud import guardar  # import absoluto
 
-print(__name__)
+    print(__name__)
 
+    def pagar_impuestos():
+        print("pagando impuestos")
+        guardar()
 
-def pagar_impuestos():
-    print("pagando impuestos")
-#    guardar()
+if __name__ == "__main__":
+    print("tarea de mantenimiento")
