3856d0ce168c48002953aefcfc3e76a38e860c58
cucr91 <camilocalderonrojas@gmail.com>
2023-09-05 21:56:44 -0300

    actualizacios en archivos de carpeta modulos-nativos

--- modulos-nativos/03-timedelta.py
+++ modulos-nativos/03-timedelta.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta
 
-fecha1 = datetime(2023, 1, 1) + timedelta(weeks=1)
-fecha2 = datetime(2023, 2, 1)
+fecha1 = datetime(2023, 5, 17) + timedelta(weeks=1)
+fecha2 = datetime(2023, 9, 4)
 
 delta = fecha2 - fecha1
-print(delta)
+print(delta, " ")
 print("dias", delta.days)
 print("segundo", delta.seconds)
 print("microsegundo", delta.microseconds)
--- modulos-nativos/07-plantillas.py
+++ modulos-nativos/07-plantillas.py
@@ -8,6 +8,9 @@
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from pathlib import Path
+from string import Template
 import smtplib
 
 # M  MULTIPORPOSE
@@ -15,11 +18,19 @@ import smtplib
 # M  MAIL
 # E  EXTENSION
 
+
+plantilla = Path("modulos-nativos/plantila.html").read_text()
+template = Template(plantilla)
+# cuerpo = template.substitute({"usuario": "chanchito feliz"})
+cuerpo = template.substitute(usuario="chanchito feliz")
+
+path = Path("modulos-nativos/holamundo.png")
+mime_image = MIMEImage(path.read_bytes())
 mensaje = MIMEMultipart()
 mensaje["from"] = "Camilo"
 mensaje["to"] = "camilocalderonrojas@gmail.com"
 mensaje["subject"] = "Esta es una prueba"
-cuerpo = MIMEText("Cuerpo del mensaje", )
+cuerpo = MIMEText(cuerpo, "html")
 mensaje.attach(cuerpo)
 
 with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
